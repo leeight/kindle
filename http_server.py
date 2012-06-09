8b516ba72ceb2f46ee4087dc0504795453092741
leeight <leeight@gmail.com>
2012-06-09 20:49:01 +0800

    IMPL http_server.py

---
+++ http_server.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#!-*- coding:utf-8 -*-
+#!vim: set ts=4 sw=4 sts=4 tw=100 noet:
+# ***************************************************************************
+# 
+# Copyright (c) 2012 Baidu.com, Inc. All Rights Reserved
+# $Id$ 
+# 接收请求，放到message queue里面去
+# **************************************************************************/
+
+
+
+import os
+import sys
+
+
+__author__ = 'leeight <liyubei@baidu.com>'
+__date__ = '2012/06/09 20:21:29'
+__revision = '$Revision$'
+
+import tornado.ioloop
+import tornado.web
+import redis
+import json
+
+class SendToKindleHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.set_header("Content-Type", "text/javascript; charset=utf-8")
+
+    title = self.get_argument("title")
+    url = self.get_argument("url")
+    version = self.get_argument("version", default="6")
+    callback = self.get_argument("callback", default="callback")
+
+    try:
+      server = redis.Redis("localhost")
+      mq_length = server.lpush("MQ_SEND_TO_KINDLE", (title, url, version))
+      self.write("%s(%s);" % (callback, json.dumps({
+          "success" : True,
+          "mq_length" : mq_length
+      })))
+    except:
+      self.write("%s(%s);" % (callback, json.dumps({
+          "success" : False,
+          "message" : "redis server was down"
+      })))
+
+class FaviconHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.redirect("http://www.baidu.com/favicon.ico", True)
+
+class ClearMQHandler(tornado.web.RequestHandler):
+  def get(self):
+    try:
+      count = 0
+      server = redis.Redis("localhost")
+      while server.llen("MQ_SEND_TO_KINDLE"):
+        server.brpop("MQ_SEND_TO_KINDLE")
+        count += 1
+      self.write("clear %s items" % count)
+    except:
+      pass
+
+application = tornado.web.Application([
+  (r"/send_to_kindle", SendToKindleHandler),
+  (r"/clear_mq", ClearMQHandler),
+  (r"/favicon\.ico", FaviconHandler)
+])
+
+if __name__ == "__main__":
+  application.listen(8964)
+  tornado.ioloop.IOLoop.instance().start()
+
